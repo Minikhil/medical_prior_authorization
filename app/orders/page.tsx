4f37aa9479b6b42c96b86da5d69642a166d1c32b
Nikhil Mithani <nikhilmithani@Nikhils-Air.lan>
2025-02-17 22:33:46 -0500

    Add orders page

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import { useRouter } from 'next/navigation'
-import "./../app/app.css";
+// import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
