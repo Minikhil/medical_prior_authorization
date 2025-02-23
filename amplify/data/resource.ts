6f0bfe43e98ed29d086f7c6d53e5a620aaa1751c
Nikhil Mithani <nikhilmithani@Nikhils-Air.lan>
2025-02-23 15:47:16 -0500

    Add PriorAuthorization dynamoDB table, forgot to add in commit 54d0af17e3d6e20c3077d28a5f205ffb78e68cfd

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,47 +1,14 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
-// Define the schema for the CodenamesGames table
-// const schema = a.schema({
-//   GameSessions: a
-//     .model({
-//       GameID: a.string().required(), // Partition Key
-//       CurrentTeam: a.string().required(), // Current team (red or blue)
-//       RedCardsLeft: a.integer().required(), // Number of red cards left
-//       BlueCardsLeft: a.integer().required(), // Number of blue cards left
-//       TotalCardsLeft: a.integer().required(), // Number of total cards left
-//       Categories: a.json().required(),  // Using JSON to represent the list of categories
-//       Cards: a.json().required(),       // Using JSON to represent the list of card objects
-//     })
-//     .authorization((allow) => [
-//       allow.publicApiKey(), // Allow API key-based access
-//     ]),
-// });
-
-// // Export the schema type
-// export type GameSessionsSchema = ClientSchema<typeof schema>;
-
-// // Define and export the Amplify data configuration
-// export const gameSessionsdata = defineData({
-//   schema,
-//   authorizationModes: {
-//     defaultAuthorizationMode: "apiKey",
-//     apiKeyAuthorizationMode: {
-//       expiresInDays: 365, // API key expires after 1 year
-//     },
-//   },
-// });
-
 const schema = a.schema({
-  Order: a
+  PriorAuthorizations: a
     .model({
-      customerName: a.string().required(),
-      customerEmail: a.string().required(),
-      customerId: a.string().required(),
-      totalAmount: a.float(),
-      sku: a.string().required(), // Part SKU
-      status: a.enum(['PENDING', 'PROCESSING', 'COMPLETED', 'CANCELLED']),
-      shippingAddress: a.json().required(), // Shipping address details
-      paymentDetails: a.json(), // Payment method and transaction details
+      employeeId: a.string().required(),
+      patientName: a.string().required(),
+      patientDateOfBirth: a.date().required(),
+      status: a.enum(['PENDING', 'COMPLETED', 'SUBMITTED', 'REJECTED', 'CANCELLED']),
+      cptCodes: a.json(), // Procedure codes
+      icdCodes: a.json(), // Diagnosis codes
     })
     .authorization((allow) => [
       allow.publicApiKey(), // Allow API key-based access
